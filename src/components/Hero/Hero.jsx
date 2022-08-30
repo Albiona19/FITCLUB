e48966bb72641240753f45bd323a714299dd4b5a
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-08-30 23:14:21 +0300

    cake

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,34 @@ const Hero = () => {
   return (
     <div className='hero'>
         <div className="left-h">
-            <Header/>
+          <Header/>
+          {/* The best ad */}
+              <div className="the-best-ad">
+              <div></div>
 
+                  <span>The best Fitness club in the town</span>
+                 
+              </div>
+              {/* Hero Heading */}
+              <div className="hero-text">
+                <div className='div1'>
+                  <span className='stroke-text'>
+                    Shape
+                  </span>
+                  <span>
+                     Your
+                  </span>
+                  </div>
+                  <div className='div2'><span>
+                    Ideal Body</span></div>
+                    <div>
+                      <span>
+                      In here we will help you to shape and build your ideal body 
+                      live up your life to fullset
+
+                      </span>
+                    </div>
+              </div>
         </div>
         <div className="right-h">right side</div>
     </div>
