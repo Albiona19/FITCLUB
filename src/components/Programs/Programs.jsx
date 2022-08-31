116580744e4894826028ca8e237b0d322e24b978
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-08-31 16:02:31 +0300

    plans finished

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -13,7 +13,7 @@ const Recipes = () => {
         </div>
         <div className="program-categories">
             {programsData.map((program)=>(
-                <div className="category">
+                <div className="category" key={programsData}>
                     {program.image}
                     <span>{program.heading}</span>
                     <span>{program.details}</span>
