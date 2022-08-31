6e6ef22fa101cc2da07c893d4166474450e21ef0
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-08-31 16:15:45 +0300

    Testimonials added

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div className="Testimonials">
+        <div className="left-t">
+            <span>Testimonials</span>
+            <span>WHAT THEY</span>
+            <span className='stroke-text'>SAY ABOUT US</span>
+
+        </div>
+        <div className="right-t">
+
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -7,19 +7,19 @@ export const testimonialsData = [
     image: image1,
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
+    name: 'Selena HENDRICKSON',
     status : 'ENTREPRENEUR'
   },
   {
     image: image2,
     review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
+    name: 'Anne KEVIN',
     status: 'COACH'
   },
   {
     image : image3,
     review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
+    name: 'Lea',
     status: "CUSTOMER"
   }
 ];
